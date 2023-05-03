06a1d03222d4c683b8c9d246a39cbceae673b742
houtjkee <kyzmyksashok@gmail.com>
2023-05-03 18:00:51 +0300

    create form and slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ const navList = document.querySelector('.header__container__list');
 burgerMenu.addEventListener('click', () => {
   navList.classList.toggle('active');
 });
+
+
+$(document).ready(function () {
+  $('.slider__card').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 962,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
+  });
+});
+
